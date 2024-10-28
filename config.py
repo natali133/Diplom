dc5dcf4dbe60dca68d10209d0a45b4339dcec2c1
natali133 <nagrigoreva8@gmail.com>
2024-10-28 12:16:04 +0300

    Diplom api

---
+++ config.py
@@ -0,0 +1,3 @@
+filmid = "suggest-item-film-535341"
+
+filmid2 = "suggest-item-film-435"
--- test_ui.py
+++ test_ui.py
@@ -2,7 +2,7 @@ import pytest
 import allure
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from config import *
 
 
 @pytest.fixture()
@@ -19,7 +19,7 @@ def chrome_browser():
 def test_by_valid_symbol(chrome_browser):
     chrome_browser.get("https://www.kinopoisk.ru/")
     chrome_browser.find_element(By.NAME, "kp_query").send_keys("1+1")
-    chrome_browser.find_element(By.ID, "suggest-item-film-535341").click()
+    chrome_browser.find_element(By.ID,filmid ).click()
     assert (chrome_browser.find_element(By.CSS_SELECTOR, "span[data-tid='75209b22']").text == "1+1 (2011)"
 
 
@@ -38,7 +38,7 @@ def test_by_invalid_symbol(chrome_browser):
 def test_search_by_russian_name(chrome_browser):
     chrome_browser.get("https://www.kinopoisk.ru/")
     chrome_browser.find_element(By.NAME, "kp_query").send_keys("Зеленая миля")
-    chrome_browser.find_element(By.ID, "suggest-item-film-435").click()
+    chrome_browser.find_element(By.ID, filmid2).click()
     assert chrome_browser.find_element(By.CSS_SELECTOR, "span[data-tid='75209b22']").text == "Зеленая миля (1999)"
 
 @allure.title("Поиск фильма по названию на латинице")
